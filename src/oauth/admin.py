405c3a02255f8bfdbd743d0d2697a82e33fcf51c
bhanuprakash-1 <prakash.2@iitj.ac.in>
2019-03-17 00:27:11 +0530

    added farmer and expert model

--- src/oauth/admin.py
+++ src/oauth/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
 from .models import Farmer, Expert
 
-
-class something(admin.ModelAdmin):
-    fieldsets = [
-        ('Name', {'fields': ['first_name', 'last_name']}),
-    ]
-
-
 admin.site.register(Expert)
-admin.site.register(Farmer, something)
+admin.site.register(Farmer)
--- src/oauth/models.py
+++ src/oauth/models.py
@@ -1,34 +1,94 @@
-from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import User
 from versatileimagefield.fields import VersatileImageField
+from django.core.validators import RegexValidator, MaxValueValidator, MinValueValidator
 
-
-# use phone number validators
 
 class Farmer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_no = models.CharField(max_length=10, null=False, blank=False, unique=True, )
-    aadhar_id = models.CharField(max_length=12, null=False, blank=False, unique=True,
-                                 help_text="Enter your Aadhar 12 digit", default="NA")
-    age = models.CharField(max_length=3, help_text="Enter your age")
-    # media directory to be set
+    full_name=models.CharField(max_length=255,default="")
+    phone = models.CharField(max_length=15,blank=True, help_text="Enter mobile number")
+    farmer = models.BooleanField(default=True)
+    expert = models.BooleanField(default=False)
+    dob = models.DateField()
+    # aadhar_validator = RegexValidator(r'^[0-9]{12}$', message='Not a valid Aadhar  number!')
+    aadhar = models.BigIntegerField()
     img_file = VersatileImageField('Image', upload_to='media/farmers_images/')
+    land_area = models.FloatField(max_length=4, blank=False, help_text="Land In Acres")
+    registration_date = models.DateTimeField(auto_now_add=True)
 
-    # Also can add type of land , size of land etc.
+    """  Add location of land ....  """
 
     def __str__(self):
-        return self.user.get_full_name()
+        return self.full_name
 
 
 class Expert(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_no = models.CharField(max_length=10, null=False, blank=False, unique=True)
-    email_id = models.EmailField(max_length=254)
-    age = models.CharField(max_length=3, help_text="Enter your age")
+    full_name = models.CharField(max_length=255, default="")
+    phone = models.CharField(max_length=15,blank=True, help_text="Enter mobile number")
+    farmer = models.BooleanField(default=False)
+    expert = models.BooleanField(default=True)
+    dob= models.DateField()
+    registration_date = models.DateTimeField(auto_now_add=True)
+    email_id = models.EmailField(max_length=254, blank=False, help_text="Enter you email id")
+    skills = models.TextField()
 
-    # media directory to be set
+    """ How to save image file with name of user?    """
 
     img_file = VersatileImageField('Image', upload_to='media/experts_images/')
 
     def __str__(self):
-        return self.user.get_full_name()
+        return self.full_name
+
+
+"""
+class AbstractUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    active = models.BooleanField(default=True)
+     phone number validators 
+    phone_validator = RegexValidator(r'^\+?1?\d{9,15}$', message='Invalid mobile number.')
+    phone_no = models.IntegerField(phone_validator, help_text="Enter correct mobile number")
+     to differentiate whether expert or farmer
+    farmer = models.BooleanField(default=False)
+    expert = models.BooleanField(default=False)
+    age = models.PositiveSmallIntegerField(validators=[MaxValueValidator(200), MinValueValidator(10)],
+                                           help_text="Enter correct age")
+    date = models.DateField(default=True, auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Farmer(AbstractUser):
+    aadhar_validator = RegexValidator(r'^[0-9]{12}$', message='Not a valid Aadhar  number!')
+    aadhar = models.BigIntegerField(validators=aadhar_validator)
+    # media directory to be set
+    img_file = VersatileImageField('Image', upload_to='media/farmers_images/')
+    land_area = models.FloatField(max_length=4, blank=False, help_text="In hectares")
+
+    def __str__(self):
+        return self.user
+
+    def is_farmer(self):
+        return self.farmer
+
+    def is_expert(self):
+        return self.expert
+
+
+class Expert(AbstractUser):
+    email_id = models.EmailField(max_length=254, blank=False, help_text="Enter you email id")
+    skills = models.TextField()
+    img_file = VersatileImageField('Image', upload_to='media/experts_images/')
+
+    def __str__(self):
+        return self.user
+
+    def is_farmer(self):
+        return self.farmer
+
+    def is_expert(self):
+        return self.expert
+
+"""
