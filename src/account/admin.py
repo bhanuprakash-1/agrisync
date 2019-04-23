2d03195e561cfd93095ce40236c5d631c5aac523
Hiren <chalodiya.1@iitj.ac.in>
2019-04-24 03:01:13 +0530

    chage in admin panel of account app

--- src/account/admin.py
+++ src/account/admin.py
@@ -4,10 +4,16 @@ from account.models import FarmerAccount, ExpertAccount
 
 class FarmerAdmin(admin.ModelAdmin):
     model = FarmerAccount
+    list_display = ('__str__', 'state', 'major_crop_list', 'email_confirmed', 'phone_confirmed')
+    list_filter = ('email_confirmed', 'phone_confirmed')
+    list_editable = ('email_confirmed', 'phone_confirmed')
 
 
 class ExpertAdmin(admin.ModelAdmin):
     model = ExpertAccount
+    list_display = ('__str__', 'state', 'skills_list', 'email_confirmed', 'phone_confirmed')
+    list_filter = ('email_confirmed', 'phone_confirmed')
+    list_editable = ('email_confirmed', 'phone_confirmed')
 
 
 admin.site.register(FarmerAccount, FarmerAdmin)
