e4e0374760d514dedfbe550636ce06c8938a116c
Hiren <chalodiya.1@iitj.ac.in>
2019-04-24 02:28:15 +0530

    add robots.txt

--- src/agrisync/urls.py
+++ src/agrisync/urls.py
@@ -9,7 +9,7 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
+Including another URL conf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.contrib.auth.views import LogoutView
 from django.urls import include
 from django.conf import settings
-from django.views.generic import RedirectView
+
 from adminsettings.admin import adminsettings
 from account.views import LoginView
 
@@ -27,12 +27,9 @@ admin.site.index_title = settings.INDEX_TITLE
 admin.site.site_title = settings.SITE_TITLE
 admin.site.site_header = settings.SITE_HEADER
 
-favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
-
 urlpatterns = [
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^logout/$', LogoutView.as_view(next_page='login'), name='logout'),
-    url(r'^favicon.ico$', favicon_view),
     url(r'^admin/', admin.site.urls),
     url(r'^admin/settings/', adminsettings.urls),
     url(r'^account/', include('account.urls', namespace='account'), kwargs={'MAINTENANCE': 'OAUTH_APP_MAINTENANCE'}),
@@ -43,3 +40,5 @@ urlpatterns = [
 if settings.DEBUG:  # pragma: no cover
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static('/favicon.ico', document_root=settings.STATICFILES_DIRS[0] + "/favicon.ico")
+    urlpatterns += static('/robots.txt', document_root=settings.STATICFILES_DIRS[0] + "/robots.txt")
