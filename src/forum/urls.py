33ae5285629c181d989def645762c40b5a15fd53
anmol-itachi <reddy.5@iitj.ac.in>
2019-04-24 17:45:23 +0530

    Forum done add topic and index

--- src/forum/urls.py
+++ src/forum/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 from .views import IndexView,TopicCreateView
-
+from forum import views
 
 app_name = 'forum'
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^topic/add/$', TopicCreateView.as_view(), name='add_topic'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^topic/add/$', views.TopicCreateView.as_view(), name='add_topic'),
+
+    url(r'^(?P<slug>[\w-]+)/$', views.TopicDetailView.as_view(), name='detail'),
 ]
+
